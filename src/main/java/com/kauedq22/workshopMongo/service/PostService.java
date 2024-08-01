91806b52d3c8db29439268b82c18d11d28f49a0c
Kauedq22 <kauedq12@gmail.com>
2024-08-01 18:25:44 -0300

    Getting post by id

---
+++ src/main/java/com/kauedq22/workshopMongo/service/PostService.java
@@ -0,0 +1,26 @@
+package com.kauedq22.workshopMongo.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kauedq22.workshopMongo.models.Post;
+import com.kauedq22.workshopMongo.repository.PostRepository;
+import com.kauedq22.workshopMongo.service.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+    
+    @Autowired
+    private PostRepository postRepository;
+
+ 
+
+    public Post findById(String id) {
+		Optional<Post> obj = postRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+
+
+}
